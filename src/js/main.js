8a0bf5adaf989fd5323778d5a7b9d674500bfb1e
Alex <yuriev200@gmail.com>
2025-03-04 13:27:16 +0200

    4 section done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded Scripts");
 });
+
+
+const processSlider = new Swiper('#processSlider', {
+  slidesPerView: 4,
+  spaceBetween: 35,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const reviewSlider = new Swiper('#reviewSlider', {
+  slidesPerView: 3,
+  spaceBetween: 35,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
